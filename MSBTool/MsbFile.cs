9309e60c770db3ecec645a132428ae4c7e9c4a2b
goat <fakecake@icloud.com>
2019-11-03 21:57:30 +0900

    Initial commit

---
+++ MSBTool/MsbFile.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MSBTool
+{
+    class MsbFile
+    {
+        public enum MsbNote
+        {
+
+        }
+
+        public class MsbBar
+        {
+            public uint Length { get; set; }
+            public uint Offset { get; set; }
+            public byte Note { get; set; }
+        }
+
+        public class MsbScore
+        {
+            public List<MsbBar> Bars { get; set; }
+        }
+
+        public class MsbBpm
+        {
+            public byte Measure1 { get; set; }
+            public byte Measure2 { get; set; }
+
+            public float Bpm { get; set; }
+        }
+
+        public List<MsbScore> ScoreEntries { get; private set; }
+        public List<MsbBpm> BpmEntries { get; private set; }
+
+        public static MsbFile Read(Stream stream)
+        {
+            var file = new MsbFile();   
+
+            file.ScoreEntries = new List<MsbScore>();
+
+            using (var reader = new BinaryReader(stream))
+            {
+                if (reader.ReadInt32() != 0x4642534D) // MSBF
+                    throw new ArgumentException("Not a MSB file", nameof(stream));
+
+                stream.Position++; //?
+
+                var scoreCount = reader.ReadByte();
+                var headerSize = reader.ReadUInt16();
+
+                var musicLength = reader.ReadUInt32();
+
+                stream.Position += 4;
+
+                var extendedHeaderSize = reader.ReadUInt32();
+                stream.Position = extendedHeaderSize; // Extended header, maybe instruments for ensembles?
+
+
+
+                // BPM HEADER
+                stream.Position += 4;
+                var bpmHeaderCount = reader.ReadUInt32();
+
+                stream.Position += 8;
+
+                file.BpmEntries = new List<MsbBpm>();
+                for (var bpmIndex = 0; bpmIndex < bpmHeaderCount; bpmIndex++)
+                {
+                    var bpm = new MsbBpm();
+
+                    stream.Position++; //?
+
+                    bpm.Measure1 = reader.ReadByte();
+                    bpm.Measure2 = reader.ReadByte();
+
+                    stream.Position++;
+
+                    bpm.Bpm = reader.ReadSingle();
+
+                    reader.ReadUInt32(); // Unknown
+                    reader.ReadUInt32(); 
+
+                    file.BpmEntries.Add(bpm);
+                }
+
+
+                for (var i = 0; i < scoreCount; i++)
+                {
+                    var score = new MsbScore();
+                    score.Bars = new List<MsbBar>();
+
+                    stream.Position += 4;
+
+                    var noteCount = reader.ReadUInt32();
+
+                    stream.Position += 8;
+
+                    for (var noteIndex = 0; noteIndex < noteCount; noteIndex++)
+                    {
+                        var bar = new MsbBar();
+
+                        stream.Position++; //?
+
+                        bar.Note = reader.ReadByte();
+
+                        stream.Position += 2; //?
+
+                        bar.Offset = reader.ReadUInt32();
+                        bar.Length = reader.ReadUInt32();
+
+                        reader.ReadUInt32(); // Unknown
+
+                        score.Bars.Add(bar);
+                    }
+
+                    file.ScoreEntries.Add(score);
+                }
+            }
+
+            return file;
+        }
+
+        // This is just terrible but it works
+        public byte[] GetBytes()
+        {
+            using (var stream = new MemoryStream(0x21C0))
+            {
+                using (var writer = new BinaryWriter(stream))
+                {
+                    writer.Write(0x4642534D);
+
+                    writer.Write((byte) 0x00);
+
+                    writer.Write((byte) ScoreEntries.Count); // Should be 7, anything else is undefined
+
+                    writer.Write((ushort) 0x0010); // Base header len
+
+                    writer.Write((uint) 105714); // Music length, lets hardcode for now
+
+                    writer.Write((uint) 0x00000000);
+
+                    writer.Write(new byte[]
+                    {
+                        0x30, 0x00, 0x00, 0x00, 0x50, 0x00, 0x00, 0x00, 0xC0, 0x03, 0x00, 0x00, 0xD0, 0x0A, 0x00, 0x00,
+                        0xE0, 0x17, 0x00, 0x00, 0x70, 0x1A, 0x00, 0x00, 0x70, 0x1C, 0x00, 0x00, 0x60, 0x1F, 0x00, 0x00
+                    }); // Extended header?
+
+
+                    // BPM
+
+                    writer.Write((uint) 0x00101000); //???
+
+                    writer.Write((uint) 0x00000001); // BPM Entries
+                    writer.Write((uint) 0x00000000);
+                    writer.Write((uint) 0x00000000);
+
+                    writer.Write((byte) 0x00);
+
+                    writer.Write((byte) BpmEntries[0].Measure1);
+                    writer.Write((byte) BpmEntries[0].Measure2);
+
+                    writer.Write((byte)0x00);
+
+                    writer.Write(BpmEntries[0].Bpm);
+
+                    writer.Write((uint)0x00000000);
+                    writer.Write((uint)0x00000000);
+
+
+
+                    foreach (var scoreEntry in ScoreEntries)
+                    {
+                        // SCORE HEADER
+                        writer.Write((uint) 0x01101001); // TODO: figure out what these mean
+
+                        writer.Write((uint) scoreEntry.Bars.Count);
+
+                        writer.Write((uint)0x00000000);
+                        writer.Write((uint)0x00000000);
+
+                        foreach (var bar in scoreEntry.Bars)
+                        {
+                            writer.Write((byte)0x00);
+                            writer.Write((byte) bar.Note);
+                            writer.Write((ushort) 0x0000);
+
+                            writer.Write((uint) bar.Offset);
+                            writer.Write((uint)bar.Length);
+
+                            writer.Write((uint)0x00000000);
+                        }
+                    }
+
+                    return stream.GetBuffer();
+                }
+            }
+        }
+    }
+}
---
+++ MSBTool/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using Melanchall.DryWetMidi.Common;
+using Melanchall.DryWetMidi.MusicTheory;
+using Melanchall.DryWetMidi.Smf;
+using Melanchall.DryWetMidi.Smf.Interaction;
+using Note = Melanchall.DryWetMidi.Smf.Interaction.Note;
+
+namespace MSBTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var file = MsbFile.Read(File.Open("bgm_score_003.msb", FileMode.Open));
+
+            /*
+            var midiFile = new MidiFile();
+
+            var tempoMap = TempoMap.Create(new TicksPerQuarterNoteTimeDivision(960),
+                Tempo.FromBeatsPerMinute((int) file.BpmEntries[0].Bpm));
+
+            foreach (var score in file.ScoreEntries)
+            {
+                var track = new TrackChunk();
+
+                var scoreNotes = new List<Note>();
+                
+                foreach (var bar in score.Bars)
+                {
+                    scoreNotes.Add(new Note(new SevenBitNumber((byte)(bar.Note + 30)), bar.Length, bar.Offset));
+                }
+
+                track.AddNotes(scoreNotes);
+
+                midiFile.Chunks.Add(track);
+            }
+
+            midiFile.ReplaceTempoMap(tempoMap);
+
+            //midiFile.Write("test.mid");
+            */
+
+            var midiFile = MidiFile.Read("test_stones.mid");
+
+            var notes = midiFile.GetTrackChunks().First().GetNotes();
+
+            for (var i = 0; i < notes.Count(); i++)
+            {
+                var note = notes.ElementAt(i);
+
+                Console.WriteLine($"{note.NoteNumber - 30} - {note.Time} - {note.Length}");
+
+                file.ScoreEntries[0].Bars[i].Note = (byte) (note.NoteNumber - 30);
+                file.ScoreEntries[0].Bars[i].Length = (uint) note.Length;
+                file.ScoreEntries[0].Bars[i].Offset = (uint) note.Time;
+            }
+
+            File.WriteAllBytes("test.msb", file.GetBytes());
+        }
+    }
+}
